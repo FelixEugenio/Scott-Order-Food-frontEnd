06d89d42466134872c60b08182f88279c49e3508
felix mavila <felixmacarenco@gmail.com>
2023-01-08 14:54:59 -0800

    configure api routes and make feature signout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import { createContext,ReactNode,useState} from 'react';
+import { destroyCookie} from 'nookies';
+import Router from 'next/router';
 
 type AuthContextData = {
   user:UserProps | null;
   isAuthtenticated:boolean;
   signIn:(credential:SignInProps) => Promise<void>
+  signOut: () => void;
 }
 
 type UserProps = {
@@ -24,6 +27,16 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut(){
+
+ try{
+    destroyCookie(undefined,'@nextauth.token')
+    Router.push('/')
+
+ }catch{
+  console.log("erro ao deslogar")
+ }
+}
 
 export function AuthProvider({children}:AuthProviderProps){
 
@@ -38,7 +51,9 @@ const isAuthtenticated = !!user;
 
 
     return(
-        <AuthContext.Provider value={{user,isAuthtenticated,signIn}}>
+        <AuthContext.Provider value={{user,isAuthtenticated,signIn,signOut() {
+            
+        },}}>
             {children}
         </AuthContext.Provider>
     )
